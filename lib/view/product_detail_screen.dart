85e5dedb5ed83d8ac6b17b27f81678a26d25b60a
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-09-10 22:32:59 +0500

    Product Name Added

--- lib/view/product_detail_screen.dart
+++ lib/view/product_detail_screen.dart
@@ -22,41 +22,56 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.asset('assets/images/bg.png', width: 474, height: 450,),
-          Image.asset('assets/images/bg_1.png', width: 330, height: 330),
-           Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                  width: 350,
-                  height: 310,
-                  child: PageView.builder(
-                    controller: _pageController,
-                    itemCount: productImages.length,
-                    itemBuilder: (context, index) {
-                      return Image.asset(
-                        productImages[index],
-                        width: 350,
-                        height: 350,
-                      );
-                    },
-                  ),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset('assets/images/bg.png', width: 474, height: 450,),
+               Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      width: 350,
+                      height: 310,
+                      child: PageView.builder(
+                        controller: _pageController,
+                        itemCount: productImages.length,
+                        itemBuilder: (context, index) {
+                          return Image.asset(
+                            widget.product.imagePath,
+                             fit: BoxFit.contain,
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    SmoothPageIndicator(
+                      controller: _pageController,
+                      count: productImages.length,
+                      effect: ScrollingDotsEffect(
+                        dotHeight: 10,
+                        dotWidth: 9,
+                         dotColor: Colors.white,
+                        activeDotColor: const Color(0xffF17547), 
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 12),
-                SmoothPageIndicator(
-                  controller: _pageController,
-                  count: productImages.length,
-                  effect: ExpandingDotsEffect(
-                    dotHeight: 10,
-                    dotWidth: 10,
-                    activeDotColor: Colors.black,
-                  ),
-                ),
-              ],
-            ),
+            ],
+          ),
+          SizedBox(height: 1),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Text(widget.product.name,  
+            style: const TextStyle(
+                fontSize: 22,
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+              ),),
+          ),
+
         ],
       ),
     );
