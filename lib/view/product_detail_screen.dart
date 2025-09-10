3bf5f1c1d4932ce45e0712691ee029b8c58dc10a
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-09-10 22:13:56 +0500

    Smooth Indicator Add

--- lib/view/product_detail_screen.dart
+++ lib/view/product_detail_screen.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:e_commerce/model/product_model.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
   final Product product;
 
   const ProductDetailsScreen({super.key, required this.product});
 
+  @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+   final PageController _pageController = PageController();
+
+  final List<String> productImages = [
+    'assets/images/bg_1.png',
+    'assets/images/bg_1.png',
+    'assets/images/bg_1.png',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +26,37 @@ class ProductDetailsScreen extends StatelessWidget {
         alignment: Alignment.center,
         children: [
           Image.asset('assets/images/bg.png', width: 474, height: 450,),
-          Image.asset('assets/images/bg_1.png', width: 350, height: 350),
+          Image.asset('assets/images/bg_1.png', width: 330, height: 330),
+           Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  width: 350,
+                  height: 310,
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: productImages.length,
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        productImages[index],
+                        width: 350,
+                        height: 350,
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 12),
+                SmoothPageIndicator(
+                  controller: _pageController,
+                  count: productImages.length,
+                  effect: ExpandingDotsEffect(
+                    dotHeight: 10,
+                    dotWidth: 10,
+                    activeDotColor: Colors.black,
+                  ),
+                ),
+              ],
+            ),
         ],
       ),
     );
